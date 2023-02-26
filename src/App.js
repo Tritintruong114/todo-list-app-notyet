c6f1951e76810fc4b28832001b6d7c6eb6a1a0ee
Tritintruong114 <truongtritin.bee@gmail.com>
2023-02-26 17:20:58 +0700

    today changing some in css, and make a ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>This is ToDo List App - By Bruno Truong</h1> */}
+      <div className="container-dailytask">
+        <h1 className="title">
+          Daily Task
+          <span>Get things done, one item at a time.</span>
+        </h1>
+        {/* <span>Get things done, one item at a time.</span> */}
+        <ul className="task-list">
+          <li>
+            <span className="label">Code a ReactJS App</span>
+            <div className="actions">
+              <input
+                type="checkbox"
+                className="btn-action btn-action-done"
+              ></input>
+              <button className="btn-action btn-action-delete">X</button>
+            </div>
+          </li>
+          <li>
+            <span className="label">Do a calendar libary</span>
+            <div className="actions">
+              <input
+                type="checkbox"
+                className="btn-action btn-action-done"
+              ></input>
+              <button className="btn-action btn-action-delete">X</button>
+            </div>
+          </li>
+          <li>
+            <span className="label">Learn ReactJS</span>
+            <div className="actions">
+              <input
+                type="checkbox"
+                className="tn-baction btn-action-done"
+              ></input>
+              <button className="btn-action btn-action-delete">X</button>
+            </div>
+          </li>
+        </ul>
+        <div className="filter-wrapper">
+          <label for="filter">Show incompleted tasks only</label>
+          <input type="checkbox"></input>
+        </div>
+
+        <form className="form">
+          <label>Add to the todo list</label>
+          <input type="text"></input>
+          <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
   );
 }
