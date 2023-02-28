3812f351527c949386234739503b539b29d0a537
Tritintruong114 <truongtritin.bee@gmail.com>
2023-02-28 18:00:46 +0700

    it okay we are update the calendar. and the timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,21 +1,59 @@
 import "./timer.css";
 import React, { useState, useEffect } from "react";
+var timer;
 
 const Timer = () => {
   const [second, setSecond] = useState(0);
   const [minutes, setMinutes] = useState(0);
 
-  useEffect(() => {});
+  useEffect(() => {
+    //
+    timer = setInterval(() => {
+      setSecond(second + 1);
+      if (second === 59) {
+        setMinutes(minutes + 1);
+        setSecond(0);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  });
+
+  const restart = () => {
+    setSecond(0);
+    setMinutes(0);
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+  };
+
+  const start = () => {};
 
   return (
-    <div className="timer">
-      <div className="container">
-        <div className="timer_container">
-          <h1>Timer</h1>
-          <h1>10:30</h1>
+    // <div className="timer">
+    <div className="timer_main">
+      <div className="timer_container">
+        <h1 className="timer_title">Timer</h1>
+        <h2 className="timer_counter">
+          {minutes < 10 ? "0" + minutes : minutes}:
+          {second < 10 ? "0" + second : second}
+        </h2>
+        <div className="buttonContainer">
+          <button className="restart" onClick={restart}>
+            Restart
+          </button>
+          <button className="stop" onClick={stop}>
+            Stop
+          </button>
+          <button className="stop" onClick={start}>
+            Start
+          </button>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 
