18a4dee01831743ecb82e92a00f1fe8eb0af502b
Tritintruong114 <truongtritin.bee@gmail.com>
2023-03-01 15:32:19 +0700

    changing css of timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from "./logo.svg";
-import background from "./background.jpeg"
+// import background from "./background.jpeg";
 import "./App.css";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
@@ -51,66 +51,68 @@ function App() {
   const [showInComplete, setShowInComplete] = useState(false);
   const [newTask, setNewTask] = useState("");
   return (
-    <div className="App">
-    <image src=""
-      {/* <h1>This is ToDo List App - By Bruno Truong</h1> */}
-      <div className="container-dailytask">
-        <h1 className="title">
-          Daily Task
-          <span>Get things done, one item at a time.</span>
-        </h1>
-        {/* <span>Get things done, one item at a time.</span> */}
-        <ul className="task-list">
-          {tasks
-            .filter((task) => (showInComplete ? task.status !== 1 : true))
-            .map((task) => (
-              <li key={task.id} className={task.status ? "done" : ""}>
-                <span className="label">{task.title}</span>
-                <div className="actions">
-                  <input
-                    type="checkbox"
-                    className="btn-action btn-action-done"
-                    checked={Boolean(task.status)}
-                    onChange={(e) => setTaskStatus(task.id, e.target.checked)}
-                  ></input>
-                  <button
-                    onClick={() => removeTask(task.id)}
-                    className="btn-action btn-action-delete"
-                  >
-                    X
-                  </button>
-                </div>
-              </li>
-            ))}
-        </ul>
-        <div className="filter-wrapper">
-          <label htmlFor="filter">Show incompleted tasks only</label>
-          <input
-            type="checkbox"
-            id="filter"
-            checked={showInComplete}
-            onChange={(e) => setShowInComplete(e.target.checked)}
-          ></input>
+    <div className="bigContainer">
+      <div className="app">
+        {/* <img className="backgroundImage" src={background} alt="" /> */}
+        {/* <h1>This is ToDo List App - By Bruno Truong</h1> */}
+        <div className="container-dailytask">
+          <h1 className="title">
+            Daily Task
+            <span>Get things done, one item at a time.</span>
+          </h1>
+          {/* <span>Get things done, one item at a time.</span> */}
+          <ul className="task-list">
+            {tasks
+              .filter((task) => (showInComplete ? task.status !== 1 : true))
+              .map((task) => (
+                <li key={task.id} className={task.status ? "done" : ""}>
+                  <span className="label">{task.title}</span>
+                  <div className="actions">
+                    <input
+                      type="checkbox"
+                      className="btn-action btn-action-done"
+                      checked={Boolean(task.status)}
+                      onChange={(e) => setTaskStatus(task.id, e.target.checked)}
+                    ></input>
+                    <button
+                      onClick={() => removeTask(task.id)}
+                      className="btn-action btn-action-delete"
+                    >
+                      X
+                    </button>
+                  </div>
+                </li>
+              ))}
+          </ul>
+          <div className="filter-wrapper">
+            <label htmlFor="filter">Show incompleted tasks only</label>
+            <input
+              type="checkbox"
+              id="filter"
+              checked={showInComplete}
+              onChange={(e) => setShowInComplete(e.target.checked)}
+            ></input>
+          </div>
+
+          <form onSubmit={handleSubmit} className="form">
+            <label htmlFor="newitem">Add to the todo list</label>
+            <input
+              type="text"
+              id="newitem"
+              value={newTask}
+              onChange={handleInputChange}
+            ></input>
+            <button type="submit">Add</button>
+          </form>
         </div>
 
-        <form onSubmit={handleSubmit} className="form">
-          <label htmlFor="newitem">Add to the todo list</label>
-          <input
-            type="text"
-            id="newitem"
-            value={newTask}
-            onChange={handleInputChange}
-          ></input>
-          <button type="submit">Add</button>
-        </form>
-      </div>
+        <div className="calender-react">
+          <Calendar onChange={onChange} value={date} />
+        </div>
 
-      <div className="calender-react">
-        <Calendar onChange={onChange} value={date} />
-      </div>
-
-      <div className="timer-react">
-        <Timer />
+        <div className="timer-react">
+          <Timer />
+        </div>
       </div>
     </div>
   );
