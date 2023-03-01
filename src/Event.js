4bb4171f2d8910a3884b29c36d13150e9fd3a26e
Tritintruong114 <truongtritin.bee@gmail.com>
2023-03-01 16:28:16 +0700

    Finish the Event adding css

--- src/Event.js
+++ src/Event.js
@@ -4,17 +4,28 @@ import event from "./event.css";
 const Event = () => {
   return (
     <div className="eventContainer">
-      <h2>Incoming Event</h2>
+      <h2 className="eventTilte">Incoming Event</h2>
       <ul className="eventUnderList">
-        <li>
+        <li className="singleList">
           Event
           <label>23/3/20</label>
         </li>
-        <li>
+        <li className="singleList">
           Event
           <label>23/3/20</label>
         </li>
       </ul>
+
+      <form className="eventInfomationContainer">
+        <label className="newEvent" htmlFor="newEvent">
+          Add Event
+        </label>
+        <div className="addEventContainer">
+          <input className="event inputEvent"></input>
+          <input className="date inputEvent"></input>
+          <button className="eventAddBtn">Add</button>
+        </div>
+      </form>
     </div>
   );
 };
