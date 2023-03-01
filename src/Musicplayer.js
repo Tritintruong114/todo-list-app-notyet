973e374e692ad9e07643b134ae26104d9fc9bd04
Tritintruong114 <truongtritin.bee@gmail.com>
2023-03-01 16:46:39 +0700

    Add music Static code , but not import in App yet

---
+++ src/Musicplayer.js
@@ -0,0 +1,42 @@
+// import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import musicplayer from "./musicplayer.css";
+
+const Music = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Music</h2>
+          <i>search</i>
+        </div>
+
+        <div>
+          <img src="" alt="#"></img>
+        </div>
+      </div>
+
+      <div>
+        <i>Rank</i>
+        <i>Singer</i>
+        <i>Radio</i>
+        <i>Style</i>
+      </div>
+      <div>
+        <div>
+          <h2>Hot Selection</h2>
+          <p>more</p>
+        </div>
+        <div>
+          <div>
+            <img src="#" alt="#"></img>
+            <p>ABC</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+};
+
+export default Music;
